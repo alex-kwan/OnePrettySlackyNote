e19b415f97351ea2da49dfadeb4dd1bdb9948256
Alex Kwan <alexkwan089@gmail.com>
2016-04-06 01:40:36 -0700

    attempting to get page name

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.post('/', function(request, response) {
       var url = request.body['text'];
       
 
-//var decodedUrl = decodeURI(url);
-//var first = decodedUrl.indexOf(".one|");
-//var second = decodedUrl.indexOf("/", first);
-//var third = decodedUrl.indexOf("|", second);
-var actualName = url.substr(3,10);//decodedUrl.substr(second, third-second);
+var decodedUrl = decodeURI(url);
+var first = decodedUrl.indexOf(".one|");
+var second = decodedUrl.indexOf("/", first);
+var third = decodedUrl.indexOf("|", second);
+var actualName = decodedUrl.substr(second, third-second);
 
 var name = actualName+" (Webview)";
       var responseUrl = request.body['response_url'];
