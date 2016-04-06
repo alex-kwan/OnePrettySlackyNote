357fb0dcf1a69ad7b22fb17932156be828c554f6
Alex Kwan <alexkwan089@gmail.com>
2016-04-06 00:01:06 -0700

    adding in some dummy command code

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.post('/', function(request, response) {
+  if(request.param('command') == "/onenoteurl"){
+      var url = request.param('text');
+      response.send("<a href="+url+">test url</a>");
+  }
 });
 
 app.listen(app.get('port'), function() {
