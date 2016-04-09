57286de5c7a9ae33169af753c273251f3ddde87b
Alex Kwan <alexkwan089@gmail.com>
2016-04-09 11:07:57 -0700

    forgot to respond back with something :p

--- index.js
+++ index.js
@@ -47,7 +47,11 @@ var name = actualName+" (Webview)";
         }
     ]
 };
-}});
+}
+     response.send(returnVal);  
+}
+ 
+);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
